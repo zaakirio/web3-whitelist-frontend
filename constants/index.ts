4d59eb66c1a926725ab2d5d62710034e52789639
unknown <rubinkazanmusic@gmail.com>
2022-05-06 17:55:42 +0100

    Added stock image for splash screen

---
+++ constants/index.ts
@@ -0,0 +1,66 @@
+export const abi = [
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_maxAddresses",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [],
+    name: "addAddress",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    name: "allowedAddresses",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "maxAddresses",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "numAddresses",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+];
+export const WHITELIST_CONTRACT_ADDRESS = "0xFE91AF5652dcf7d8Ea77754126Ff22a8B4501b8f";
